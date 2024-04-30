0b47dd5c5990a3c4dbc6e799652f12ac06f0dc51
Muhammad Adeel Shakir <captainadeel000@gmail.com>
2024-04-30 10:08:57 +0500

    Fromted created and data inserting successfully

--- insert.php
+++ insert.php
@@ -6,41 +6,105 @@
     <title>Registration Form</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+ <link rel="stylesheet" href="./Bootstrap/css/bootstrap.min.css">
+ <script src="./Bootstrap/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
 
 <!-- view categories container -->
-<div class="mt-5 offset-3 col-lg-6 bg-white shadow p-4">
-<div class="offset-4 col-lg-4 mb-4">
-                <label for=""></label>
+<div class="mt-5  col-lg-10 offset-1 bg-white shadow p-4">
 
-                <button class="btn btn-info text-white btn-lg mt-2 w-100"> User Information</button>
-            </div>
+
+        <!-- view categories container -->
+        
+            <h3 class=" mt-2 text-success"> User Information</h3>
+           
+            <hr>
 
             
-    <div class="form-container">
+            <div class="form-container">
+                <form  class="row">
 
-            <div class="col-lg-8 offset-2 mb-2">
-                <label class="form-label" for="email">Email <span class="text-danger">*</span>
-                </label>
-                <input type="email" class="form-control" id="email" name="email" placeholder="Enter here..." required>
+                    <div class="col-lg-6 mb-2">
+                        <label class="form-label"><h6>First Name </h6></label>
+                        <input type="text" class="form-control" id="fname"  placeholder="Enter here..." required>
+                    </div>
+
+
+                    <div class="col-lg-6 mb-2">
+                        <label class="form-label"><h6>Last Name </h6></label>
+                        <input type="text" class="form-control" id="lname"  placeholder="Enter here..." required>
+                    </div>
+                    <div class="offset-8 col-lg-4 ">
+                        
+
+                        <button class="btn btn-success text-white btn-lg mt-1 w-100" id="send" > Add User</button>
+                    </div>
+
+                    
+
+        <!-- view categories container -->
+        <div class="container mt-3 bg-white p-4">
+            <h3 class="text-success">  View Users</h3>
+            <hr>
+
+
+            <div class="table-responsive">
+                <table class="table table-striped table-bordered zero-configuration">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
             </div>
 
 
-            <div class="col-lg-8 offset-2 mb-2">
-                <label class="form-label" for="password">Password <span class="text-danger">*</span>
-                </label>
-                <input type="password" class="form-control" id="password" name="password" placeholder="Enter here..." required>
-            </div>
+</form>
 
-    
+</div>
 
-            <div class="offset-4 col-lg-4 mb-2">
-                <label for=""></label>
+ <!-- js links -->
+    <script src="./jquery/jquery-3.7.1.min.js"></script>
 
-                <button class="btn btn-info text-white btn-lg mt-2 w-100"  id="submit" value="submit">Submit</button>
-            </div>
+    <script>
+        $(document).ready(function() {
+
+            var btn = $("#send");
+
+            // insert data function 
+            btn.click(function() {
+                var fname = $("#fname").val();
+                var lname = $("#lname").val();
+
+                // send data on insert qry page
+                $.ajax({
+                    url: "./insert-query.php",
+                    type: "POST",
+                    data: {
+                        firstName: fname,
+                        lastName: lname
+                    },
+                    success: function(response) {
+                        alert(response);
+
+                        $("#fname").val("");
+                        $("#lname").val("");
+                    }
+                })
+
+
+            })
+
+        })
+    </script>
+
           
 
 
